52c111174bb12989ec9e9c592b6374afa3f9d8f0
Aisling <aismcauley00@gmail.com>
2021-01-22 21:32:07 +0000

    update deploy file

--- landing-page/deploy.sh
+++ landing-page/deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:ais93/landing-page.git master:gh-pages
+git push -f git@github.com:ais93/LandingPage.git master:gh-pages
 cd -
